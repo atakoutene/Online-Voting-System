a8df5dbca633a5b4ecab5852230c8271065e684c
Pacha <atakoutene@gmail.com>
2023-07-16 10:42:15 +0100

    creating backend directory abd setting up configurations by Aurelien Takou

---
+++ backend/online-voting-system/src/test/java/com/pacha/onlinevotingsystem/OnlineVotingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pacha.onlinevotingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineVotingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
